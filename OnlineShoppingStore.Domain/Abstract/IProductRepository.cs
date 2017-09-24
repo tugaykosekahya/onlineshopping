921fd5ded8594fbe28a4946c0eef02e9e2b7d34d
Tugay <tugaykosekahya@outlook.com>
2017-09-25 00:17:32 +0300

    Add files via upload

---
+++ OnlineShoppingStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShoppingStore.Domain.Entities;
+
+namespace OnlineShoppingStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productId);
+    }
+}
---
+++ OnlineShoppingStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "ripondatta.us@gmail.com";
+        public string MailFromAddress = "ripondatta.us@gmail.com";
+        public bool UseSsl = true;
+        public string Username = "ripondatta.us@gmail.com";
+        public string Password = ""; // Create your own google app password, In the video I have shown you how to create app password.
+        public string ServerName = "smtp.gmail.com";
+        public int ServerPort = 587;
+    }
+}
